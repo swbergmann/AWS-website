f6c52af8f3e53b98ecafda2b44a924856c5d6f15
Research <research@Researchs-MBP.fritz.box>
2024-04-14 11:44:35 +0200

    comment the code

--- main.tf
+++ main.tf
@@ -1,18 +1,14 @@
 locals {
-  s3_bucket_name = "website-backend-2384209"
+  # value must be globally unique
+  s3_bucket_name = "website-backend-6571963"
 }
 
-# resource "aws_instance" "instance1" {
-#     ami = "ami-0f7204385566b32d0"
-#     instance_type = "t2.micro"
-# }
-
-# must be empty to be deleted
-resource "aws_s3_bucket" "main" {
+# empty the S3 bucket manually before it can be deleted by terraform
+resource "aws_s3_bucket" "backend" {
   bucket = local.s3_bucket_name
 }
 
-resource "aws_cloudfront_distribution" "main" {
+resource "aws_cloudfront_distribution" "frontend" {
   enabled = true
   default_root_object = "index.html"
   is_ipv6_enabled = true
@@ -22,14 +18,14 @@ resource "aws_cloudfront_distribution" "main" {
     allowed_methods = [ "GET", "HEAD", "OPTIONS" ]
     cached_methods = [ "GET", "HEAD", "OPTIONS" ]
     cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
-    target_origin_id = aws_s3_bucket.main.bucket
+    target_origin_id = aws_s3_bucket.backend.bucket
     viewer_protocol_policy = "redirect-to-https"
   }
 
   origin {
-    domain_name = aws_s3_bucket.main.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.main.id
-    origin_id = aws_s3_bucket.main.bucket
+    domain_name = aws_s3_bucket.backend.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+    origin_id = aws_s3_bucket.backend.bucket
   }
 
   restrictions {
@@ -43,14 +39,18 @@ resource "aws_cloudfront_distribution" "main" {
   }
 }
 
-resource "aws_cloudfront_origin_access_control" "main" {
-  name = "s3-cloudfront-oac"
+# restrict access to and make S3 backend more secure
+# cloudfront distribution uses this OAC with an S3 as origin
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name = "cloudfront-oac-to-S3"
   origin_access_control_origin_type = "s3"
   signing_behavior = "always"
   signing_protocol = "sigv4"
 }
 
-data "aws_iam_policy_document" "cloudfront_oac_access" {
+# describes the IAM policy
+# is expected by the S3 bucket policy (resource below)
+data "aws_iam_policy_document" "document" {
   statement {
     principals {
       identifiers = [ "cloudfront.amazonaws.com" ]
@@ -58,24 +58,25 @@ data "aws_iam_policy_document" "cloudfront_oac_access" {
     }
 
     actions = ["s3:GetObject"]
-    resources = [ "${aws_s3_bucket.main.arn}/*" ]
+    resources = [ "${aws_s3_bucket.backend.arn}/*" ]
 
     condition {
       test = "StringEquals"
-      values = [ aws_cloudfront_distribution.main.arn ]
+      values = [ aws_cloudfront_distribution.frontend.arn ]
       variable = "AWS:SourceArn"
     }
   }
 }
 
-#apply policy to S3 bucket
-resource "aws_s3_bucket_policy" "main" {
-  bucket = aws_s3_bucket.main.id
-  policy = data.aws_iam_policy_document.cloudfront_oac_access.json
+#apply the IAM policy to the S3 bucket
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.backend.id
+  policy = data.aws_iam_policy_document.document.json
 }
 
-resource "aws_s3_object" "upload_source_files" {
-  bucket = aws_s3_bucket.main.id
+#automatically upload the files to S3
+resource "aws_s3_object" "auto_upload" {
+  bucket = aws_s3_bucket.backend.id
 
   for_each = fileset("s3_content/", "**/*.*")
 
